a6f1bbbe15d765a26cf2ab3264343ca8a1e5af38
owner <yangbrian14@gmail.com>
2021-02-03 17:10:11 -0700

    Just wanted to add another line.

--- HelloFromKisuk/src/Main.java
+++ HelloFromKisuk/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello, my name is Kisuk");
         System.out.println("Hi, Kisuk. This is Brian.");
+        System.out.println("HI, I just wanted to add another line.");
     }
 
 }
